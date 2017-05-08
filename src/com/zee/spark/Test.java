1c4d812dc743106390ece96c39b1eec05a58a05f
zee1159 <zee1159@gmail.com>
2017-05-08 17:04:56 -0500

    Fixed file read for RDD

--- src/com/zee/spark/Test.java
+++ src/com/zee/spark/Test.java
@@ -1,10 +1,16 @@
 package com.zee.spark;
 
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 import org.apache.spark.api.java.*; 
 
 public class Test {
 	public static String path;
+	
+	private static final Pattern NEWLINE = Pattern.compile("\n");
 	
 	public native String run(String path, String fname);
 	
@@ -22,16 +28,21 @@ public class Test {
 		
 		path = args[0];
 		
-		JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+		//JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 		
-		JavaRDD<String> data = jsc.textFile(args[1]);
+		JavaRDD<String> lines = spark.read().textFile(args[1]).javaRDD();
+		//JavaRDD<String> data = jsc.textFile(args[1]);
 		
-		JavaRDD<String> compute = data.map(s->(compute(s)));
+		JavaRDD<String> img = lines.flatMap(s->Arrays.asList(NEWLINE.split(s)).iterator());
+		
+		JavaRDD<String> compute = img.map(s->(compute(s)));
 		
 		compute.saveAsTextFile(args[2]);
+		//jsc.close();
+		spark.stop();
 	}
 
-	private static String compute(String s) {
+	public static String compute(String s) {
 		// TODO Auto-generated method stub
 		Test app = new Test();
 		//String in_path = path + "/data/";
